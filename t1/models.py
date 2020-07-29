1f4ac470950c7652ae086486fedcbb0cd77269af
Nagesh <nageshrewatkar16@gmail.com>
2020-07-29 09:47:04 +0530

    Task1
    signup login page

---
+++ t1/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class EmployeeModel(models.Model):
+
+	idno		=models.AutoField(primary_key=True)
+	name		=models.CharField(max_length=30)
+	contact_no	=models.IntegerField(unique=True)
+	email		=models.CharField(max_length=100,unique=True)
+	password	=models.CharField(max_length=20)
+	# emp_photo   =models.FileField(upload_to='my_document/')
+
